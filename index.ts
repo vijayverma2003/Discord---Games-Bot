3bb4235b453049ceb94e088c42b3e2f3843c7632
Vijay <vijayvermakvp@gmail.com>
2024-01-21 09:46:51 +0530

    Refactoring Commands

--- index.ts
+++ index.ts
@@ -1,8 +1,6 @@
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
-import end from "./text-commands/end";
-import help from "./text-commands/help";
-import start from "./text-commands/start";
+import commands from "./text-commands";
 
 dotenv.config();
 
@@ -15,20 +13,14 @@ const client = new Client({
   ],
 });
 
-export type Games = Collection<
-  string,
-  Collection<string, number | boolean | Collection<string, number> | string[]>
->;
-
-const games: Games = new Collection();
-
 const prefix = "v.";
 
 client.on(Events.MessageCreate, async (message) => {
   try {
-    const { content, channelId, author } = message;
-    if (!content.toLowerCase().startsWith(prefix) || message.author.bot) return;
-    const args = content.slice(prefix.length).trim().split(/ +/);
+    if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot)
+      return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift();
 
     if (!command) {
@@ -36,9 +28,23 @@ client.on(Events.MessageCreate, async (message) => {
       return;
     }
 
-    if (command === "end") end(message);
-    if (command === "start") start(message, args);
-    if (command === "help") help(message);
+    switch (command) {
+      case "start":
+        commands.start(message, args);
+        break;
+
+      case "help":
+        commands.help(message);
+        break;
+
+      case "end":
+        commands.end(message);
+        break;
+
+      default:
+        await message.channel.send("Invalid Command");
+        return;
+    }
   } catch (error) {
     console.log(error);
     await message.channel.send("An error occured while executing the command.");
---
+++ text-commands/index.ts
@@ -0,0 +1,11 @@
+import help from "./help";
+import start from "./start";
+import end from "./end";
+
+const commands = {
+  help,
+  start,
+  end,
+};
+
+export default commands;
